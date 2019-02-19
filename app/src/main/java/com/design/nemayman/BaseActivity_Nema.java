b01e60cf2bac9ec288a82e53ddaa1a89998d4009
alireza <abedzadehalireza74@gmail.com>
2019-02-19 15:16:33 +0330

    font Commit

---
+++ app/src/main/java/com/design/nemayman/BaseActivity_Nema.java
@@ -0,0 +1,17 @@
+package com.design.nemayman;
+
+import android.app.Application;
+
+import com.design.nemayman.Classes.TypefaceUtil;
+
+public class BaseActivity_Nema extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/Sahel-FD.ttf"); // font from assets: "assets/fonts/Roboto-Regular.ttf
+
+    }
+
+}
