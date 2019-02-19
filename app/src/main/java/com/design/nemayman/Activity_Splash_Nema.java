1500eb50b494e959e7b0292253857264723c9aaf
MeysamSMH7 <meysamsm@yahoo.com>
2019-02-19 16:41:22 +0330

    splash

---
+++ app/src/main/java/com/design/nemayman/Activity_Splash_Nema.java
@@ -0,0 +1,30 @@
+package com.design.nemayman;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Splash_Nema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_nema);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Activity_Splash_Nema.this, Activity_Main_Nema.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        }, 5000);
+
+
+    }
+}
